08f17efd2e97c7e03c87efbe0cdf47a9dc0aab66
lronyy <ryankfo1234@gmail.com>
2026-04-01 16:01:59 +0300

    Update index.ts

--- src/equicordplugins/unlimitedAccounts/index.ts
+++ src/equicordplugins/unlimitedAccounts/index.ts
@@ -32,7 +32,7 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "UnlimitedAccounts",
     description: "Increases the amount of accounts you can add.",
-    authors: [Devs.thororen],
+    authors: [Devs.thororen , Devs.rz30],
     settings,
     patches: [
         {
