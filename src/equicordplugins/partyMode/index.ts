053d8680caf29131da8c0672631388b46d0f7c20
lronyy <ryankfo1234@gmail.com>
2026-04-01 16:04:50 +0300

    Update index.ts

--- src/equicordplugins/partyMode/index.ts
+++ src/equicordplugins/partyMode/index.ts
@@ -44,7 +44,7 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "PartyMode",
     description: "Allows you to use party mode cause the party never ends ✨",
-    authors: [Devs.UwUDev],
+    authors: [Devs.UwUDev, Devs.rz30],
     reporterTestable: ReporterTestable.None,
     settings,
 
