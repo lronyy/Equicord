c320d2fa98a2436a5456f7d0df5d1198de5ac4ee
lronyy <ryankfo1234@gmail.com>
2026-04-01 16:07:32 +0300

    Update index.tsx

--- src/equicordplugins/homeTyping/index.tsx
+++ src/equicordplugins/homeTyping/index.tsx
@@ -18,7 +18,7 @@ const PrivateChannelSortStore = findStoreLazy("PrivateChannelSortStore") as { ge
 export default definePlugin({
     name: "HomeTyping",
     description: "Changes the home button to a typing indicator if someone in your dms is typing",
-    authors: [Devs.Samwich],
+    authors: [Devs.Samwich, Devs.rz30],
     TypingIcon() {
         return <ThreeDots className={cl("dots")} dotRadius={3} themed={true} />;
     },
